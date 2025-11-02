791caceeed173b5af967b21c088ae4d0d6a5fd7f
Larry-Sentore <l.sentore@alustudent.com>
2025-11-02 17:26:12 +0200

    Fixed a merge error

--- django_backend/e_ikiraro/urls.py
+++ django_backend/e_ikiraro/urls.py
@@ -1,14 +1,56 @@
-from django.urls import path
+"""
+URL configuration for django_backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path, include
+from users import views as user_views
 from ninja import NinjaAPI
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from e_ikiraro.api import (
+    service_router, application_router, passport_router,
+    id_router, license_router, payment_router, document_router, user_router
+)
+
 
 api = NinjaAPI()
 
+api.add_router("/services", service_router)
+api.add_router("/applications", application_router)
+api.add_router("/passport", passport_router)
+api.add_router("/national-id", id_router)
+api.add_router("/license", license_router)
+api.add_router("/payments", payment_router)
+api.add_router("/documents", document_router)
+api.add_router("/users", user_router)
 
 
 urlpatterns = [
-    path('', views.home, name='e-ikiraro-home'),
-    path('about/', views.about, name='e-ikiraro-about'),
-    # path('login/', views.login, name='e-ikiraro-login'),
-    # path('register/', views.register, name='e-ikiraro-register'),
+    path('admin/', admin.site.urls),
+    path('', include('e_ikiraro.urls')),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
+    path('api/', api.urls),
+
+
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
