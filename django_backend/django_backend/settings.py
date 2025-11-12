71d922fe6053749c2343b93fa8ca774d3aabfd46
terrydestroye <s.amuzu@alustudent.com>
2025-11-12 16:20:41 +0200

    moved the keys to a .env file and updated requirements.txt and .gitignore

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gsf1^&j40z0ip6y^bcm%l8-xym-dfip+$^rlv0hdc=t2b@xh66'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -85,7 +86,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -124,7 +124,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
-    ]
+]
 
 
 # Default primary key field type
@@ -139,20 +139,17 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'sedemkofiamuzu@gmail.com'
 EMAIL_HOST_USER = 'sedemkofiamuzu@gmail.com'
-EMAIL_HOST_PASSWORD = 'njjfqwspxkremrue'
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-#4hours
+# 4hours
 PASSWORD_RESET_TIMEOUT = 14400
-
-
-
 
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
-CRISPY_TEMPLATE_PACK = 'bootstrap5' 
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'e-ikiraro-home'
 LOGIN_URL = 'login'
