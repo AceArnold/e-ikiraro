d763211611ae3c20cd2e9d1e2aff11916547b678
Cynthia Keza <c.keza@alustudent.com>
2025-11-14 00:52:22 +0200

    Add Kirundi and French translations

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -129,13 +129,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+from django.utils.translation import gettext_lazy as _
+import os 
+
+LANGUAGE_CODE = 'en'
+
+LANGUAGES = [
+    ('fr', _('French')),
+    ('rn', _('Kirundi')),
+
+]
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+
+]
 
 
 # Static files (CSS, JavaScript, Images)
